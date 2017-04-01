cf957c4d25829f6bff96c1412fd2d6e8d8b06c76
teamwsiz <pmarecki.pro@gmail.com>
2017-04-01 10:31:43 +0200

    Podstawy Javy: pętle i zmienne

---
+++ src/zajecia2/podstawy_petle/A.java
@@ -0,0 +1,27 @@
+package zajecia2.podstawy_petle;
+
+/**
+ * Created by pm on 2017-04-01.
+ */
+public class A {
+    public static void main(String[] args) {
+        String s = "abc";
+        System.out.println(s);
+        System.out.println(s + " -- " + s);
+
+        int x = 10;
+        int y = 12;
+        int z = x + y;
+
+        System.out.println(x);
+        System.out.println("x = " + x);
+        System.out.println("x*y jest rowne " + (x*y));
+
+        for(int i = 0; i<=10; i++) {
+
+            System.out.println("napis" + i);
+        }
+
+
+    }
+}
