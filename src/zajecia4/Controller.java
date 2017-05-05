7bf9aa5dbc18e1276cb2ba123ef786a9e30e49d1
teamwsiz <pmarecki.pro@gmail.com>
2017-05-05 23:10:20 +0200

    Style css

---
+++ src/zajecia4/Controller.java
@@ -0,0 +1,17 @@
+package zajecia4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+
+import java.util.UUID;
+
+public class Controller {
+    @FXML
+    Label labelka;
+
+    public void foo() {
+        System.out.println("hi!");
+    }
+
+}
---
+++ src/zajecia4/Main.java
@@ -0,0 +1,31 @@
+package zajecia4;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("fourth.fxml"));
+
+//        root.setStyle("-fx-background-image: url('resources/background.jpg')");
+//        root.setStyle(this.getClass().getResource("style.css").toExternalForm());
+
+        primaryStage.setTitle("Zajęcia 4");
+        Scene scene = new Scene(root, 500, 500);
+//        scene.getStylesheets().add("resources/style.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
