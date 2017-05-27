1a16ebc3ad58314f166f03443f823c9448a7eca1
Piotr Marecki <pmarecki.pro@gmail.com>
2017-05-27 11:06:09 +0200

    Wyłączanie opcji menu

--- src/zajecia5/Controller.java
+++ src/zajecia5/Controller.java
@@ -1,12 +1,16 @@
 package zajecia5;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
 import javafx.scene.control.TextArea;
 
 
 public class Controller {
     @FXML
     TextArea oknoTekstowe;
+
+    @FXML
+    Menu toolsMenu;
 
     public void sayIt() {
         System.out.println("It");
@@ -16,5 +20,13 @@ public class Controller {
         String tekst = oknoTekstowe.getText();
         tekst = tekst.toUpperCase();
         oknoTekstowe.setText(tekst);
+
+        boolean isDisabled = toolsMenu.isDisable();
+        if (isDisabled) {
+            toolsMenu.setDisable(false);
+        } else {
+            toolsMenu.setDisable(true);
+        }
+
     }
 }
