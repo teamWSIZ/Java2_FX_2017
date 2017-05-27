96dcb01905d73009d98f6db3c3faab433be221eb
Piotr Marecki <pmarecki.pro@gmail.com>
2017-05-27 10:32:29 +0200

    BorderPane i okno tekstowe

--- src/zajecia5/Controller.java
+++ src/zajecia5/Controller.java
@@ -1,7 +1,19 @@
 package zajecia5;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+    @FXML
+    TextArea oknoTekstowe;
+
     public void sayIt() {
         System.out.println("It");
+    }
+
+    public void zmienNaDuze() {
+        String tekst = oknoTekstowe.getText();
+        tekst = tekst.toUpperCase();
+        oknoTekstowe.setText(tekst);
     }
 }
