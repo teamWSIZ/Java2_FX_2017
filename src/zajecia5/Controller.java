08420c8ddb9bca96f1b27457400d063630cdc68c
Piotr Marecki <pmarecki.pro@gmail.com>
2017-05-29 23:42:57 +0200

    OpenFile dialog; stage; organization of buttons

--- src/zajecia5/Controller.java
+++ src/zajecia5/Controller.java
@@ -1,5 +1,6 @@
 package zajecia5;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -7,6 +8,10 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Menu;
 import javafx.scene.control.TextArea;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 
 public class Controller {
@@ -19,6 +24,9 @@ public class Controller {
     @FXML
     Canvas mycanvas;
 
+    @FXML
+    Stage stage;
+
     public void sayIt() {
         System.out.println("It");
     }
@@ -27,14 +35,18 @@ public class Controller {
         String tekst = oknoTekstowe.getText();
         tekst = tekst.toUpperCase();
         oknoTekstowe.setText(tekst);
+    }
 
+    public void disableToolsMenu() {
         boolean isDisabled = toolsMenu.isDisable();
         if (isDisabled) {
             toolsMenu.setDisable(false);
         } else {
             toolsMenu.setDisable(true);
         }
+    }
 
+    public void drawOnCanvas() {
         //Użycie canvasu:
         GraphicsContext gc = mycanvas.getGraphicsContext2D();
         int size = 30;
@@ -43,13 +55,10 @@ public class Controller {
                 gc.fillRoundRect(5 + i * 50, 5 + j * 50, size, size, 10, 10);
             }
         }
-//        Rectangle rr = new Rectangle(10, 10, 200, 100);
-//        gc.
-
-
     }
 
     public void alertujUsera() {
+
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle("Warning Dialog!");
         alert.setHeaderText("Some header");
@@ -57,5 +66,10 @@ public class Controller {
         alert.setContentText("Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam erat ac ipsum. Integer aliquam purus. ");
         alert.showAndWait();
         //more at: http://code.makery.ch/blog/javafx-dialogs-official/
+    }
+
+    public void myOpenFile() {
+        File f = new FileChooser().showOpenDialog(stage);
+        System.out.println(f.getAbsolutePath());
     }
 }
