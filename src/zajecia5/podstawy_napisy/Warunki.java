5a626545849cdb9f62a6836037eb218b6cb717bf
Piotr Marecki <pmarecki.pro@gmail.com>
2017-06-03 11:31:11 +0200

    Warunki

---
+++ src/zajecia5/podstawy_napisy/Warunki.java
@@ -0,0 +1,22 @@
+package zajecia5.podstawy_napisy;
+
+public class Warunki {
+    public static void main(String[] args) {
+        int x = 6;
+
+        //jesli jest podzielne przez 2 to napisz "dwa", jeśli nie, ale podzielne przez 3 to napisz 3
+        if (x%2==0) {
+            System.out.println("dwa");
+        } else if (x%3==0) {
+            System.out.println("trzy");
+        }
+
+        //jesli podzielne przez 2 to napisz "2", dodatkowo jesli podzielne przez 3 to napisz 3
+        if (x%2==0) {
+            System.out.println("2");
+        }
+        if (x%3==0) {
+            System.out.println("3");
+        }
+    }
+}
