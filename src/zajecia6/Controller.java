733b960e7374435b850856eb93b450abc6ee1dea
Piotr Marecki <pmarecki.pro@gmail.com>
2017-06-10 11:32:10 +0200

    Rotated image

--- src/zajecia6/Controller.java
+++ src/zajecia6/Controller.java
@@ -1,17 +1,27 @@
 package zajecia6;
 
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.ArcType;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.util.Pair;
 
 import javax.swing.*;
@@ -73,23 +83,66 @@ public class Controller {
     public void drawOnCanvas() {
         //Użycie canvasu:
         GraphicsContext gc = mycanvas.getGraphicsContext2D();
-        int size = 30;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                gc.fillRoundRect(5 + i * 50, 5 + j * 50, size, size, 10, 10);
-            }
-        }
+
         //see eg. http://docs.oracle.com/javafx/2/canvas/jfxpub-canvas.htm
         gc.setFill(Color.color(0.1, 0.7, 0.5, 0.5));
         gc.fillArc(10, 110, 300, 300, 45, 240, ArcType.OPEN);
-        //todo: draw image
-//        Image lulu = loadImage("Lulu-Dragon-Trainer-icon.png");
-//        gc.drawImage(lulu, 30, 100, 256, 256);
-
+        int size = 128;
         for (int i = 0; i < ikony.size(); i++) {
-            gc.drawImage(ikony.get(i), 50 + i * 135, 50, 128, 128);
+            gc.drawImage(ikony.get(i), 50 + i * 135, 55, size, size);
+            gc.strokeRoundRect(50 + i * 135, 50, size, size, 10, 10);
         }
 
+        //todo: załadować część obrazka
+        //todo: ustawić rodzaj pędzla
+        //todo: obsługa myszy i "draggable"
+
+
+
+
+    }
+
+    public void animateLulu() {
+        GraphicsContext gc = mycanvas.getGraphicsContext2D();
+        DoubleProperty x  = new SimpleDoubleProperty();
+        DoubleProperty y  = new SimpleDoubleProperty();
+        DoubleProperty phi = new SimpleDoubleProperty();
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(
+                        Duration.seconds(0),
+                        new KeyValue(x, 0),
+                        new KeyValue(y, 0),
+                        new KeyValue(phi, 0)
+                ),
+                new KeyFrame(Duration.millis(2500),
+                        new KeyValue(x, 200),
+                        new KeyValue(y, 200),
+                        new KeyValue(phi, 180)
+                )
+        );
+        timeline.setAutoReverse(true);
+        timeline.setCycleCount(12);
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                Image i = ikony.get(0);
+
+                ImageView iv = new ImageView(i);
+                iv.setRotate(phi.getValue());
+                SnapshotParameters params = new SnapshotParameters();
+                params.setFill(Color.TRANSPARENT);
+                Image rotatedImage = iv.snapshot(params, null);
+
+                gc.clearRect(0, 0, 500, 500);
+                //show icon on canvas
+                gc.drawImage(rotatedImage, x.doubleValue(), y.doubleValue(), 300, 300);
+
+            }
+        };
+        timer.start();
+        timeline.play();
     }
 
     public void alertujUsera() {
