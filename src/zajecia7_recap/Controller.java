ad166ed7f0f2172e31c329e374a5266f9cf3a556
pmarecki <pmarecki@gmail.com>
2017-09-02 11:01:11 +0200

    Pokaż obrazek

--- src/zajecia7_recap/Controller.java
+++ src/zajecia7_recap/Controller.java
@@ -1,7 +1,14 @@
 package zajecia7_recap;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
 public class Controller {
+    @FXML
+    Canvas mycanvas;
 
 
     public void sayIt() {
@@ -14,5 +21,12 @@ public class Controller {
 
     public void alertujUsera() {
         System.out.println("alert!");
+    }
+
+    public void showPicture() {
+        Image veigar = new Image(getClass().getResourceAsStream("res/Veigar-icon.png"));
+        GraphicsContext gc = mycanvas.getGraphicsContext2D();
+        gc.drawImage(veigar, 10, 50, 250, 250);
+
     }
 }
